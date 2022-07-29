e066a40225a374358734b16a6826fa73ccf6f65d
Cody Tolene <codyatolene@gmail.com>
2022-07-29 03:02:59 -0500

    Update package imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgxGistModule } from './public/ngx-gist.module';
 import { BodyComponent, FooterComponent, HeaderComponent } from './layout';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, BodyComponent, FooterComponent, HeaderComponent],
-  imports: [BrowserModule, NgxGistModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+    NgxGistModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/public/ngx-gist.module.ts
+++ src/app/public/ngx-gist.module.ts
@@ -4,8 +4,6 @@ import { NgxGistComponent } from './ngx-gist.component';
 import { NgxGistService } from './ngx-gist.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GistContentPipe } from './ngx-gist-content.pipe';
 import { GistFileFilterPipe } from './ngx-gist-file-filter.pipe';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,9 +11,11 @@ import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [NgxGistComponent, GistContentPipe, GistFileFilterPipe],
   imports: [
-    BrowserAnimationsModule,
+    // Needs to be imported at root.
+    // BrowserAnimationsModule,
     CommonModule,
-    HttpClientModule,
+    // Needs to be imported at root.
+    // HttpClientModule,
     MatCardModule,
     MatIconModule,
     MatTabsModule,
